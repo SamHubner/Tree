05fb60b9c21d09f32fd221ed4e522c57ec208c55
Sam Hubner <K038639@eltham-college.org.uk>
2021-05-05 11:33:32 +0100

    Add project files.

---
+++ Tree/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Node
+    {
+        public Node left;
+        public Node right;
+        public int number;
+    }
+}
---
+++ Tree/Tree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Tree
+    {
+        private Node root;
+
+        public int add(x)
+        {
+            x = new Node()
+
+
+        }
+
+
+
+    }
+
+}
